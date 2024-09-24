5668d9613892737e18c9a038b5a312b7d6bec08d
NicolasNewman <nanewman01@gmail.com>
2024-09-24 14:22:09 -0500

    feat(site): turned the course cards into a component

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,13 +1,9 @@
 // import { useState } from 'react';
 import './App.css';
 import { catalog } from 'data';
-import { Card, Select, Tooltip } from 'antd';
-import {
-    BookOutlined,
-    HomeOutlined,
-    PaperClipOutlined,
-} from '@ant-design/icons';
+import { Select } from 'antd';
 import { useState } from 'react';
+import CourseCard from './components/CourseCard';
 
 type SortType = 'reviews' | 'workload' | 'rating' | 'difficulty';
 type OrderType = 'asc' | 'dsc';
@@ -74,76 +70,7 @@ function App() {
                     .sort((a, b) => sortFunc[sortBy](a, b, orderBy))
                     .map(data => {
                         console.log(data);
-                        return (
-                            <div key={data.slug}>
-                                <Tooltip title={data.name} placement="top">
-                                    <Card
-                                        hoverable
-                                        title={`${data.codes[0]}: ${data.name}`}
-                                        style={{ width: 300 }}
-                                        actions={[
-                                            data.officialURL ? (
-                                                <a href={data.officialURL}>
-                                                    <HomeOutlined />
-                                                </a>
-                                            ) : (
-                                                <div className="h-[22px]"></div>
-                                            ),
-                                            data.textbooks?.[0].url ? (
-                                                <a
-                                                    href={
-                                                        data.textbooks?.[0].url
-                                                    }
-                                                >
-                                                    <BookOutlined />
-                                                </a>
-                                            ) : (
-                                                <div className="h-[22px]"></div>
-                                            ),
-                                            data.notesURL ? (
-                                                <a href={data.notesURL}>
-                                                    <PaperClipOutlined />
-                                                </a>
-                                            ) : (
-                                                <div className="h-[22px]"></div>
-                                            ),
-                                        ]}
-                                    >
-                                        {/* <div>{data?.description ?? ''}</div> */}
-                                        <div className="grid grid-cols-2">
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                Reviews:
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                {' '}
-                                                {data.reviewCount}
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                Workload:
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                {data.workload?.toFixed(2) ??
-                                                    'NaN'}
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                Rating:
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                {data.rating?.toFixed(2) ??
-                                                    'NaN'}
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                Difficulty:
-                                            </div>
-                                            <div className="odd:text-end odd:mr-2 even:ml-2 even:text-start">
-                                                {data.difficulty?.toFixed(2) ??
-                                                    'NaN'}
-                                            </div>
-                                        </div>
-                                    </Card>
-                                </Tooltip>
-                            </div>
-                        );
+                        return <CourseCard data={data} />;
                     })}
             </div>
             <div className="mt-8">
