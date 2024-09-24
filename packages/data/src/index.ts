343643de1da23a1ec3ee538b1ea9c5dd80a8f369
NicolasNewman <nanewman01@gmail.com>
2024-09-24 14:14:41 -0500

    feat(data): better typing for catalog

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,7 +1,7 @@
-import catalog from './catalog';
+import _catalog from './catalog';
 import getCourse from './loader';
 
-type CourseKeys = keyof typeof catalog;
+type CourseKeys = keyof typeof _catalog;
 
 type Course = {
     creditHours: number;
@@ -28,6 +28,52 @@ type Review = {
     difficulty: number;
     workload: number;
 };
+
+type CatalogEntry = {
+    tags: string[];
+    _updatedAt: string;
+    isFoundational: boolean;
+    isDeprecated: boolean;
+    slug: string;
+    _createdAt: string;
+    _id: string;
+    programs: {
+        _ref: string;
+        _type: string;
+        _key: string;
+    }[];
+
+    codes: string[];
+    textbooks?: {
+        url: string;
+        name: string;
+        _key: string;
+    }[];
+    syllabus: {
+        file: {
+            _type: string;
+            asset: {
+                _ref: string;
+                _type: string;
+            };
+        };
+    };
+    _type: string;
+    name: string;
+    _rev: string;
+    id: string;
+    description: string;
+    creditHours: number;
+    officialURL?: string;
+    notesURL?: string;
+    reviewCount: number;
+    rating?: number;
+    difficulty?: number;
+    workload?: number;
+};
+
+// @ts-ignore
+const catalog: Record<CourseKeys, CatalogEntry> = _catalog;
 
 export { catalog, getCourse };
 export type { Course, Review, CourseKeys };
