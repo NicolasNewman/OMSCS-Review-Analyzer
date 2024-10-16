531a73ba94ca4e4f27f55cc0f40a3635d497d25e
NicolasNewman <nanewman01@gmail.com>
2024-10-16 10:35:02 -0500

    fix: conditional base path based on env

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: 'https://nicolasnewman.github.io/OMSCS-Review-Analyzer/',
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react()],
+        base: env.NODE_ENV === 'development' ? '/' : '/OMSCS-Review-Analyzer/',
+    };
 });
